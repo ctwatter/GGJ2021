e6714f4215a139b1229b159bef3e37f7c19bbcae
Eugene Shin <shin.eugene97@gmail.com>
2021-01-30 15:40:29 -0800

    scene setup

---
+++ GGJ2021/Assets/Scripts/DragObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+
+
+public class DragObject : MonoBehaviour
+{
+
+    private Vector3 mOffset;
+    private Vector3 mousePos;
+    private float mZCoord;
+
+    void OnMousePosition(InputValue value)
+    {
+        mousePos= value.Get<Vector2>();
+        
+    }
+
+    void OnMouseDown()
+
+    {
+
+        mZCoord = Camera.main.WorldToScreenPoint(
+
+            gameObject.transform.position).z;
+
+
+
+        // Store offset = gameobject world pos - mouse world pos
+
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+
+    }
+
+
+    private Vector3 GetMouseAsWorldPoint()
+
+    {
+        // z coordinate of game object on screen
+        mousePos.z = mZCoord;
+
+        // Convert it to world points
+
+        return Camera.main.ScreenToWorldPoint(mousePos);
+
+    }
+
+    void OnMouseDrag()
+
+    {
+
+        transform.position = GetMouseAsWorldPoint() + mOffset;
+
+    }
+
+}
