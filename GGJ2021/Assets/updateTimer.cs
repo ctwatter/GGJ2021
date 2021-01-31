f1aa8ef8929f30b8ff1b86b2d49136dd58fb0de7
Colin Watterson <ctwatterson123@gmail.com>
2021-01-30 21:27:17 -0800

    last updates

---
+++ GGJ2021/Assets/updateTimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class updateTimer : MonoBehaviour
+{
+    
+    public TextMeshProUGUI text;
+    void Start()
+    {
+        text.text = "You only wasted " + PersitentData.Instance.globalTimeLeft + " seconds on this. Splendid.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
